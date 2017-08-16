eabd44b929cb164d5a350a185cc4dda93e684ee7
Alejandro Sanabria <alesanabriav@gmail.com>
2017-08-16 10:27:42 -0500

    move functions to actions

--- actions/bands.js
+++ actions/bands.js
@@ -1,2 +1,31 @@
-const TYPE = 'COMPANIES';
-const endpoint = '/api/v1/companies';
+const TYPE = 'BAND';
+const endpointItunes = 'https://itunes.apple.com';
+const endpointBandsintown = 'https://rest.bandsintown.com';
+
+searchVideos(query) {
+  request
+  .get(`${endpointItunes}/search?term=${query}&entity=musicVideo&limit=8`)
+  .then(res => res.data)
+  .then(res => this.setState({videos: res.results}));
+}
+
+searchMusic(query) {
+  request
+  .get(`${endpointItunes}/search?term=${query}&entity=musicVideo&limit=8`)
+  .then(res => res.data)
+  .then(res => this.setState({videos: res.results}));
+}
+
+searchEvents(query) {
+  request
+  .get(`${endpointBandsintown}/artists/${query}/events?app_id=developersoul`)
+  .then(res => res.data)
+  .then(res => console.log(res));
+}
+
+searchArtist(query) {
+  request
+  .get(`${endpointBandsintown}/artists/${query}/?app_id=developersoul`)
+  .then(res => res.data)
+  .then(res => this.setState({artist: res}));
+}
