a5d5bbe6e6a37a68ed8a25499538e5a7ab7481bf
Alejandro Sanabria <alesanabriav@gmail.com>
2017-08-17 12:18:39 -0500

    add test events

---
+++ tests/components/events.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Events from '../../components/events';
+
+describe('Events component', () => {
+  it('should render items', () => {
+    const props = {items: [
+      {
+        id: 1,
+        venue: {
+          name: 'some place',
+          country: 'Colombia',
+          country: 'Bogotá',
+          datetime: '07-07-2017'
+        }
+      },
+      {
+        id: 2,
+        venue: {
+          name: 'some place',
+          country: 'Colombia',
+          country: 'Bogotá',
+          datetime: '07-08-2017'
+        }
+      }
+    ]};
+    
+    const component = shallow(<Events {...props}/>);
+    expect(component.find('.card--event').length).toEqual(2);
+  })
+});
